651d07fa8b6cf0b9d65b4cce8c15d06796c2fa16
halfonso <hector.alfonso@yahoo.com>
2019-12-30 17:12:43 -0500

    asd

--- Altuiz_ExcelImport/Program.cs
+++ Altuiz_ExcelImport/Program.cs
@@ -1,31 +1,30 @@
-﻿using System;
+﻿using CitiBank_Excel_Import.Models;
+using LinqToExcel;
+using LinqToExcel.Domain;
+using LinqToExcel.Query;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CitiBank_Excel_Import.Models;
-using LinqToExcel;
-using LinqToExcel.Domain;
-using LinqToExcel.Query;
-using Remotion;
 
-namespace CitiBank_Excel_Import
+namespace Altuiz_ExcelImport
 {
     class Program
     {
-        private static string connectionString;        
+        private static string connectionString;
 
         static void Main(string[] args)
         {
 
             ReadConfigfile();
 
-            ProcessFiles(GetFiles()).GetAwaiter().GetResult();
+            ProcessFiles(GetFiles());
 
             Console.WriteLine("Process Complete");
-            Console.ReadLine();
+            Console.Read();
         }
 
         private static ImportFile[] GetFiles()
@@ -40,7 +39,7 @@ namespace CitiBank_Excel_Import
                 var cmd = new SqlCommand(sql, cn);
                 {
                     cn.Open();
-                    
+
                     var reader = cmd.ExecuteReader();
 
                     while (reader.Read())
@@ -49,10 +48,10 @@ namespace CitiBank_Excel_Import
                         {
                             FilePath = reader.GetString(0),
                             TableName = reader.GetString(1),
-                            Action = reader.GetString(2)                           
-                           });
+                            Action = reader.GetString(2)
+                        });
                     }
-                    
+
 
                     cn.Close();
                 }
@@ -61,20 +60,20 @@ namespace CitiBank_Excel_Import
             return files.ToArray();
         }
 
-        private static async Task ProcessFiles(ImportFile[] files)
+        private static void ProcessFiles(ImportFile[] files)
         {
-            
+
             foreach (var file in files)
             {
-                var exists = await TableExists(file.TableName);
+                var exists = TableExists(file.TableName);
 
-                if (exists) 
+                if (exists)
                 {
                     try
                     {
-                        if (await VerifyColumNamesMatch(file))
+                        if (VerifyColumNamesMatch(file))
                         {
-                            await ProcessFile(file);
+                            ProcessFile(file);
                         }
                         else
                         {
@@ -85,7 +84,7 @@ namespace CitiBank_Excel_Import
                     {
                         throw ex;
                     }
-                    
+
                 }
                 else
                 {
@@ -95,12 +94,12 @@ namespace CitiBank_Excel_Import
 
         }
 
-        private static async Task ProcessFile(ImportFile file)
+        private static void ProcessFile(ImportFile file)
         {
             var sql = "";
             var factory = new ExcelQueryFactory(file.FilePath);
             var tableName = Path.GetFileNameWithoutExtension(file.FilePath);
-            var columnNamesFromMapping = await GetTableColumnNames(file.TableName);
+            var columnNamesFromMapping = GetTableColumnNames(file.TableName);
             var columnCount = columnNamesFromMapping.Length;
             var rowsDeleted = 0;
             SqlDataReader reader = default(SqlDataReader);
@@ -118,23 +117,23 @@ namespace CitiBank_Excel_Import
                     try
                     {
                         cmd.Transaction = trans;
-                                    
+
                         if (file.Action == "I")
                         {
                             sql = $"DELETE {tableName} WHERE dttolap = '{fileUpdateDate.ToString("MM/dd/yy")}'; SELECT @@ROWCOUNT";
 
                             cmd.CommandText = sql;
 
-                            reader = await cmd.ExecuteReaderAsync();
-                            await reader.ReadAsync();
+                            reader = cmd.ExecuteReader();
+                            reader.ReadAsync();
                             rowsDeleted = reader.GetInt32(0);
                         }
 
                         var recordsInserted = 0;
                         var insertValues = "";
-                        var insertHeader = $"INSERT INTO {tableName} ";                        
-                       // insertHeader += $"({String.Join(",", columnNamesFromMapping)}, DATETIME_UPDATE, dttolap) VALUES ";
-                        
+                        var insertHeader = $"INSERT INTO {tableName} ";
+                        // insertHeader += $"({String.Join(",", columnNamesFromMapping)}, DATETIME_UPDATE, dttolap) VALUES ";
+
                         ExcelQueryable<LinqToExcel.Row> data = factory.Worksheet(0);
 
                         foreach (var row in data)
@@ -146,8 +145,12 @@ namespace CitiBank_Excel_Import
                                 insertValues += $"'{row[x].ToString().Replace("'", "''")}',";
                             }
                             insertValues = insertValues.Substring(0, insertValues.Length - 1) + ", GETDATE(), '" + fileUpdateDate.ToString("MM/dd/yy") + "');";
+
                             
                             recordsInserted++;
+
+                            //if (recordsInserted > 300)
+                            //    break;
                         }
 
                         insertValues = insertValues.Substring(0, insertValues.Length - 1);
@@ -155,28 +158,28 @@ namespace CitiBank_Excel_Import
 
                         sql = $"{insertValues}; SELECT @@ROWCOUNT";
                         // sql = $"{insertHeader} {insertValues}; SELECT @@ROWCOUNT";
-                                                
+
                         cmd.CommandText = sql;
 
                         if (reader != null && !reader.IsClosed) reader.Close();
-                                                
 
-                        reader = await cmd.ExecuteReaderAsync();
-                        await reader.ReadAsync();
 
-                       // var recordsInserted = reader.GetInt32(0);
+                        reader = cmd.ExecuteReader();
+                        reader.ReadAsync();
+
+                        // var recordsInserted = reader.GetInt32(0);
 
                         reader.Close();
 
 
 
-                        await WriteToLog(file.TableName, "INSERT", $"INSERTED {recordsInserted} record(s)", cmd);
+                        WriteToLog(file.TableName, "INSERT", $"INSERTED {recordsInserted} record(s)", cmd);
 
                         if (rowsDeleted > 0)
                         {
-                            await WriteToLog(file.TableName, "DELETE", $"DELETED {rowsDeleted} record(s)", cmd);
+                            WriteToLog(file.TableName, "DELETE", $"DELETED {rowsDeleted} record(s)", cmd);
                         }
-                        
+
                         trans.Commit();
                     }
                     catch (Exception ex)
@@ -186,12 +189,12 @@ namespace CitiBank_Excel_Import
                         throw ex;
                     }
                 }
-            }          
+            }
         }
 
-        
 
-        private static async Task<bool> VerifyColumNamesMatch(ImportFile file)
+
+        private static bool VerifyColumNamesMatch(ImportFile file)
         {
             var factory = new ExcelQueryFactory(file.FilePath);
 
@@ -201,11 +204,11 @@ namespace CitiBank_Excel_Import
                 factory.DatabaseEngine = DatabaseEngine.Ace;
 
             var columnNamesFromFile = factory.GetColumnNames(factory.GetWorksheetNames().First());
-            var columnsFromTableForExcel = await GetTableColumnNames( 
+            var columnsFromTableForExcel = GetTableColumnNames(
                 tableName: file.TableName);
 
 
-            if (await VerifyUserColumnEntryMatchesSchema(file.TableName))
+            if (VerifyUserColumnEntryMatchesSchema(file.TableName))
             {
                 foreach (var colName in columnNamesFromFile)
                 {
@@ -230,7 +233,7 @@ namespace CitiBank_Excel_Import
             return true;
         }
 
-        private static async Task<bool> VerifyUserColumnEntryMatchesSchema(string tableName)
+        private static bool VerifyUserColumnEntryMatchesSchema(string tableName)
         {
             var sql = $@"SELECT*
                             FROM(
@@ -244,20 +247,20 @@ namespace CitiBank_Excel_Import
                             AND c.name NOT IN ('dttolap', 'DATETIME_UPDATE')) s ON u.column_name = s.name
                             WHERE s.name IS NULL OR u.column_name IS NULL";
 
-            var r = await RunSqlRecordsExist(sql);
+            var r = RunSqlRecordsExist(sql);
 
             if (r)
             {
-                await WriteToLog(tableName, "", "Mismatch between table definition and table column mapping. Data was not imported.");
+                WriteToLog(tableName, "", "Mismatch between table definition and table column mapping. Data was not imported.");
             }
 
             return !r;
         }
 
 
-  
 
-        private static async Task<string[]> GetTableColumnNames(string tableName)
+
+        private static string[] GetTableColumnNames(string tableName)
         {
             var names = new List<string>();
             using (var cn = new SqlConnection(connectionString))
@@ -269,7 +272,7 @@ namespace CitiBank_Excel_Import
                 {
                     cn.Open();
 
-                    var reader = await cmd.ExecuteReaderAsync();
+                    var reader = cmd.ExecuteReader();
 
                     while (reader.Read())
                     {
@@ -283,24 +286,24 @@ namespace CitiBank_Excel_Import
             return names.ToArray();
         }
 
-        private static async Task<bool> TableExists (string tableName)
+        private static bool TableExists(string tableName)
         {
             bool exists = false;
 
             using (var cn = new SqlConnection(connectionString))
             {
                 var sql = $"SELECT * FROM sys.tables WHERE name ='{tableName}'";
-                
+
                 using (var cmd = new SqlCommand(sql, cn))
                 {
                     cn.Open();
 
-                    var reader = await cmd.ExecuteReaderAsync();
+                    var reader = cmd.ExecuteReader();
 
                     exists = reader.HasRows;
 
                     cn.Close();
-                }               
+                }
             }
 
             return exists;
@@ -309,15 +312,21 @@ namespace CitiBank_Excel_Import
         private static void ReadConfigfile()
         {
             // lines do not have to be in any specific order
-            var lines = File.ReadAllLines("config.txt");
+            var lines = File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "config.txt");
+
+
+            //connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString.ToString();
+
+
+            //  Console.WriteLine( AppDomain.CurrentDomain.BaseDirectory);
 
             connectionString = lines
                 .Where(x => x.ToLower().StartsWith("connectionstring"))
                 .First()
-                .Split(new char[] { '|' })[1];                    
+                .Split(new char[] { '|' })[1];
         }
 
-        private static async Task RunSql(string sql)
+        private static void RunSql(string sql)
         {
             using (var cn = new SqlConnection(connectionString))
             {
@@ -331,7 +340,7 @@ namespace CitiBank_Excel_Import
                     {
                         cmd.Transaction = trans;
 
-                        await cmd.ExecuteNonQueryAsync();
+                        cmd.ExecuteNonQuery();
 
                         trans.Commit();
                     }
@@ -345,21 +354,21 @@ namespace CitiBank_Excel_Import
             }
         }
 
-        private async static Task<bool> RunSqlRecordsExist(string sql)
+        private static bool RunSqlRecordsExist(string sql)
         {
             using (var cn = new SqlConnection(connectionString))
             {
                 using (var cmd = new SqlCommand(sql, cn))
                 {
                     cn.Open();
-                    var reader = await cmd.ExecuteReaderAsync(System.Data.CommandBehavior.CloseConnection);
+                    var reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
                     return reader.HasRows;
                 }
             }
         }
 
 
-        private static async Task WriteToLog(string table, string action, string message)
+        private static void WriteToLog(string table, string action, string message)
         {
             using (var cn = new SqlConnection(connectionString))
             {
@@ -376,22 +385,22 @@ namespace CitiBank_Excel_Import
                     cmd.Parameters.AddWithValue("DWH_ACTION_PARAM", action);
                     cmd.Parameters.AddWithValue("DWH_TABLE_RELATED_PARAM", table);
                     cmd.Parameters.AddWithValue("DWH_DETAIL_EVENT", message);
-                    await cmd.ExecuteNonQueryAsync();
+                    cmd.ExecuteNonQuery();
                 }
             }
-                    
+
         }
 
-        private static async Task WriteToLog(string table, string action, string message, SqlCommand cmd)
+        private static void WriteToLog(string table, string action, string message, SqlCommand cmd)
         {
             cmd.Parameters.Clear();
-            cmd.CommandText = "dbo.DWUS_SP_UPDATE_EVENT_LOG";            
+            cmd.CommandText = "dbo.DWUS_SP_UPDATE_EVENT_LOG";
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("USER_ID_PARAM", "EVG");
             cmd.Parameters.AddWithValue("DWH_ACTION_PARAM", action);
             cmd.Parameters.AddWithValue("DWH_TABLE_RELATED_PARAM", table);
             cmd.Parameters.AddWithValue("DWH_DETAIL_EVENT", message);
-            await cmd.ExecuteNonQueryAsync();                    
+            cmd.ExecuteNonQuery();
         }
     }
 }
